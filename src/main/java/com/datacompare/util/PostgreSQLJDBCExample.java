c634ac8da6c04d961667a88bd88103dea9a6e695
Madhusudan Athinarapu <95905313+amsudanaz@users.noreply.github.com>
2026-07-03 17:19:10 +0530

    security fix

--- src/main/java/com/datacompare/util/PostgreSQLJDBCExample.java
+++ src/main/java/com/datacompare/util/PostgreSQLJDBCExample.java
@@ -79,8 +79,7 @@ public class PostgreSQLJDBCExample {
 
 		if (connection != null) {
 			System.out.println("You made it, take control your database now!");
-//		} else {
-//			System.out.println("Failed to make connection!");
+
 		}
 	}
 }
